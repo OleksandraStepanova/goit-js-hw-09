980b01b9ae498c515be5eb63906eee42fc6c0179
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-03-11 12:22:13 +0100

    validator

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,9 +1,12 @@
+import validator from "validator";
+
 const KEY_MESSAGE = "feedback-form-state";
 const formRef = document.querySelector('form');
 
+
 formRef.addEventListener('input', addLocalStorage)
-function addLocalStorage() {
-    const objMessage = JSON.stringify({ email: formRef.elements.email.value, message: formRef.elements.message.value });
+function addLocalStorage() {    
+    const objMessage = JSON.stringify({ email: formRef.elements.email.value.trim(), message: formRef.elements.message.value.trim()});
     localStorage.setItem(KEY_MESSAGE, objMessage)    
 }
 
@@ -13,10 +16,15 @@ formRef.elements.message.value = objMessage.message || '';
 
 formRef.addEventListener('submit', removeLocalStorage)
 function removeLocalStorage(event) {
-    event.preventDefault(); 
-    console.log(JSON.parse(localStorage.getItem(KEY_MESSAGE)));
-    localStorage.removeItem(KEY_MESSAGE);
-    formRef.elements.email.value = '';
-    formRef.elements.message.value = '';
+    event.preventDefault();
+    if (validator.isEmpty(formRef.elements.email.value) || validator.isEmpty(formRef.elements.message.value)) {
+        alert('Заповніть будь-ласка всі поля форми!')
+    }
+    else {
+        console.log(JSON.parse(localStorage.getItem(KEY_MESSAGE)));
+        localStorage.removeItem(KEY_MESSAGE);
+        formRef.elements.email.value = '';
+        formRef.elements.message.value = '';
+    }    
 }
  
